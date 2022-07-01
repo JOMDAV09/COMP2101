3955e45be891e68aa79102a8a3ee23df448a8c36
Jomin Davis <jomindavis6655@gmail.com>
2022-07-01 02:34:22 -0400

    helloworld.sh

--- bash/test/helloworld.sh
+++ bash/test/helloworld.sh
@@ -1,9 +1,38 @@
 #!/bin/bash
 
-echo hostname: $( hostname )
-echo domain name: $( hostname -d )
-echo operating system name and version: $( cat /etc/os-release | grep PRETTY ) 
-echo Ip address :;
-ifconfig 2>stderr | grep flag | awk '{ print $1 }' | while read result ; do echo $result  $( ifconfig | grep -A4 $result | grep inet | sed s/"inet"// ) /n ; done 2>stderr 
-echo root filesystem status: ;
-df -h /
+#This is the system information of this device.
+
+
+#This command store the hostname of the device.
+hn=$(hostname)
+
+
+# This command store domain name if it have one.
+dn=$(dnsdomainname)
+
+
+#This command store the name and verson of operating system.
+#We use 2 options with the command to get the precise reading.
+os=$(lsb_release -d -s)
+
+
+#This command store ip address of the device.
+#We used grep command to select the line containing ip address and awk command to select the word from the line by passing it through the pipeline.
+ip=$(ip a s ens33 | grep -w inet | awk '{print $2}')
+
+
+#This command stores the amount of free space available in root filesystem.
+#We used df command to display the disc usage, tail to select the last line, and awk to get only the required data.
+freespace=$(df -h /dev/sda3 | tail -1 | awk '{print $4}')
+
+
+#We are calling all variables to display the result.
+cat <<EOF
+ Report for $hn
+ ======================================================================
+ FQDN: $dn
+ Operating System name and version: $os 
+ IP Address: $ip
+ Root Filesystem Free Space: $freespace
+ ======================================================================
+EOF
